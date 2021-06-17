795859d0b0a200f9e85d6ee1747e835266f3132a
Felipe Escobar R <pipesco93@gmail.com>
2021-06-16 20:42:06 -0500

    Funciones de orde superior2

--- high_order_funct.py
+++ high_order_funct.py
@@ -12,6 +12,21 @@ print(odd)
 #Map fun
 my_list2 = [1, 2, 3, 4, 5]
 
+# lambda recibe x y retorna x**2 y lo guarda en squares 
 squares = list(map(lambda x: x**2, my_list2))
 
 print(squares)
+
+#-------------------------------------------------
+
+# Reduce function
+
+from functools import reduce
+
+my_list3 = [2, 2, 2, 2, 2]
+
+# lambda primer parámetro y lista segundo parámetro
+# a * b = 4 --> 4 pasa a ser a y 4*2 = 8 y asi sigue
+all_multiplied = reduce(lambda a, b: a * b, my_list3)
+
+print(all_multiplied)
