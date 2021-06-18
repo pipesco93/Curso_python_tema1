6066eae9b40ea449eba646f10287dbd1f931da85
Felipe Escobar R <pipesco93@gmail.com>
2021-06-17 21:07:03 -0500

    nuevo archivo

---
+++ assert_statement.py
@@ -0,0 +1,13 @@
+def palindrome(string):
+    assert len(string) > 0, "No s epuede ingresar una cadena vacía"
+    return string == string[::-1]
+
+def run():
+    
+    print(palindrome(""))
+   
+    
+
+
+if __name__=='__main__':
+    run()
