0c6c6e43f25f21f602009f322661fb277b0d3c2a
Felipe Escobar R <pipesco93@gmail.com>
2021-06-19 12:15:47 -0500

    Manejo de archivos con python

--- archivos.py
+++ archivos.py
@@ -9,6 +9,7 @@ def read():
 def write():
     names = ["Facundo", "Miguel", "Pepe", "Christian", "Rocío"]
     with open("./archivos/names.txt", "w", encoding="utf-8") as f:
+        #con "w" Reescribe cada que se ejecuta con "a" =append agrega si cambio la lista no se sobreescribe
         for name in names:
             f.write(name)
             f.write("\n")
